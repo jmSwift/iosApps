ddf7968511c96ede94f55548f5391f31e4faa8be
Jose <jose@Joses-MacBook-Pro-5.local>
2016-03-26 12:08:14 -0700

    user login ui

---
+++ IM/IM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IM
+//
+//  Created by Jose on 3/26/16.
+//  Copyright © 2016 JoseApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
