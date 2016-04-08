1dbc62437ce3d6eb4e4314b99dfc38277f152144
Jose <jose@Joses-MacBook-Pro-5.local>
2016-04-07 23:58:28 -0700

    fb fixed

--- IM/IM/ViewController.swift
+++ IM/IM/ViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit // needed for FB
 
 class ViewController: UIViewController {
     
     let ref = Firebase(url: "https://myinstantmessenger.firebaseio.com")
+    // facebookLogin = FBSDKLoginManager()
 
     @IBOutlet var emailTextField: MaterialField!
     
     @IBOutlet var passwordField: MaterialField!
+    
+    @IBAction func fbBtnPressed(sender: UIButton!)
+    {
+        let facecbookLogin = FBSDKLoginManager()
+        
+        facecbookLogin.logInWithReadPermissions(["email"],fromViewController:self, handler:{ (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook Login failed \(facebookError)")
+                
+            } else {
+               let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                
+                print("successfully logged in with facebook. \(accessToken)")
+                
+                
+            }
+        
+        })
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
